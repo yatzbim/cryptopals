df96cb337d467e6ae0e214ec2f01e1ece3e6a9dd
Jonah Miller <jonahmiller5@gmail.com>
2023-03-16 09:17:05 -0700

    initialize cargo project and start first task

---
+++ src/basics/exercise-1.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+
+fn hex_to_base64(hex_string: String) -> String {
+    todo!()
+}
+
+// given an ASCII byte value, gives the corresponding number 0-15 if it matches
+fn hex_byte_to_base10(byte: u8) -> u8 {
+    todo!()
+}
+
+// given an ASCII byte value, gives the corresponding number 0-63 if it matches
+fn base64_byte_to_base10(byte: u8) -> u8 {
+    todo!()
+}
+
+fn main() -> Result<()> {
+    todo!()
+}
